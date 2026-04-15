796104e9d08bdf5bb6b27b1fca33a98717711b2c
tumaro <contact.tumins@gmail.com>
2026-04-15 17:15:54 +0530

    SOLUTIONS: ch3 exercises

--- solutions/ch3.py
+++ solutions/ch3.py
@@ -1,4 +1,4 @@
-from src.rigid_body import so3, SO3
+from src.rigid_body import so3, SO3, SE3, se3
 import numpy as np
 from modern_robotics import VecToso3, MatrixExp3, MatrixLog3
 
@@ -68,3 +68,14 @@ if __name__ == "__main__":
         [1,0,0]
     ])))
     
+    print("3.16 i>")
+    T_16 = SE3.transform(np.array([[0,-1,0],[0,0,-1],[1,0,0]]), np.array([3,0,0]))
+    w_mat, v, theta = se3.logarithm6(T_16)
+    w = so3.skew_to_vec(w_mat)
+    print(f"omega={np.round(w,4)}, v={np.round(v,4)}, theta={round(theta,4)} rad")
+    S = np.concatenate([w, v])
+    print(f"Screw axis S = {np.round(S, 4)}")
+    q, s_hat, h = se3.axis_to_screw(S)
+    print(f"q={np.round(q,4)}, s_hat={np.round(s_hat,4)}, h={round(h,4)}")
+
+    print("3.28 >")
