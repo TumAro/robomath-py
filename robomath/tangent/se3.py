77ed134c865217384f5b26ddbd88dc07bae6d2bc
tumaro <contact.tumins@gmail.com>
2026-05-01 19:47:33 +0530

    FEAT: implemented so3 + se3 tangent space

---
+++ robomath/tangent/se3.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from robomath._types import Matrix_4X4, Vector6
+
+from robomath._core.se3 import (
+    se3_test,
+    se3_to_vec
+)
+
+@dataclass (frozen=True)
+class se3:
+    matrix: Matrix_4X4
+
+    def __post_init__(self) -> None:
+        if not se3_test(self.matrix):
+            raise ValueError("Not a se3 matrix!")
+        
+    def __repr__(self) -> str:
+        return f"se3(\n{self.matrix}\n)"
+    
+    def to_vec(self) -> Vector6:
+        return se3_to_vec(self.matrix)
+    
+    def exp(self, theta: float):
+        # TODO: implement after SE3 class exists
+        raise NotImplementedError
---
+++ robomath/tangent/so3.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from robomath._types import Matrix_3x3, Vector3
+
+from robomath._core.so3 import (
+    check_skew_symmetry,
+    skew_to_vec
+)
+
+@dataclass (frozen=True)
+class so3:
+    matrix: Matrix_3x3
+
+    def __post_init__(self) -> None:
+        if not check_skew_symmetry(self.matrix):
+            raise ValueError("Not a skew symmetric matrix!")
+        
+    def __repr__(self) -> str:
+        return f"so3(\n{self.matrix}\n)"
+        
+    def to_vec(self) -> Vector3:
+        return skew_to_vec(self.matrix)
+    
+    def exp(self, theta: float):
+        # TODO: implement after SO3 class exists
+        raise NotImplementedError
