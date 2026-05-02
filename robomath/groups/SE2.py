0d839df126efa849c7d867f287dc6e82c1512f9e
tumaro <contact.tumins@gmail.com>
2026-05-02 15:14:07 +0530

    FIX: class name typo

--- robomath/groups/SE2.py
+++ robomath/groups/SE2.py
@@ -5,7 +5,7 @@ from robomath._types import Matrix_3x3
 
 
 @dataclass (frozen=True)
-class SO2(LieGroup):
+class SE2(LieGroup):
     matrix: Matrix_3x3
     # TODO: se2 tangent space is not built yet
 
